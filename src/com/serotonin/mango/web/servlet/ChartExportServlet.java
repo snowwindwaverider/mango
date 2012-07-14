f09ed472c86ba410cfe7abaaa308ff321c4d232c
snowwindwaverider <snowwindwaverider@gmail.com>
2012-07-14 12:48:19 -0700

    csv export use tabular form

--- src/com/serotonin/mango/web/servlet/ChartExportServlet.java
+++ src/com/serotonin/mango/web/servlet/ChartExportServlet.java
@@ -44,7 +44,7 @@ public class ChartExportServlet extends HttpServlet {
         response.setContentType("text/csv");
 
         final ResourceBundle bundle = Common.getBundle();
-        final ReportCsvStreamer exportCreator = new ReportCsvStreamer(response.getWriter(), bundle);
+        final ReportCsvStreamerTabular exportCreator = new ReportCsvStreamerTabular(response.getWriter(), bundle);
 
         final ReportDataValue rdv = new ReportDataValue();
         MappedRowCallback<PointValueTime> callback = new MappedRowCallback<PointValueTime>() {
